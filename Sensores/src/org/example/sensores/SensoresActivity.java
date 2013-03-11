380cefced3468480aded1faa768c2a92644c18cb
pmjmadrid <pmjmadrid@gmail.com>
2013-03-11 15:08:20 +0100

    Módulo 5 - Listar los sensores del dispositivo.
    Además de la lista de los sensores del dispositivo, se ha incluido 
    también acceder a los datos de cada uno de los sensores que aparezca
    en la lista de sensores

--- Sensores/src/org/example/sensores/SensoresActivity.java
+++ Sensores/src/org/example/sensores/SensoresActivity.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import android.app.Activity;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
 
-public class SensoresActivity extends Activity {
+public class SensoresActivity extends Activity implements SensorEventListener {
 	
 	private TextView salida;
 	
@@ -23,6 +25,41 @@ public class SensoresActivity extends Activity {
 		for (Sensor sensor: listaSensores){
 			log(sensor.getName());
 		}
+		
+		/**
+		 * Para obtener la lectura de cada uno de los sensores
+		 * se añade el siguiente código, ue 
+		 */
+		
+		 listaSensores = sensorManager.getSensorList(Sensor.TYPE_ORIENTATION);
+
+		 if (!listaSensores.isEmpty()) {
+		        Sensor orientationSensor = listaSensores.get(0);
+		        sensorManager.registerListener(this, orientationSensor, SensorManager.SENSOR_DELAY_UI);
+		 }
+
+		 listaSensores = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+
+		 if (!listaSensores.isEmpty()) {
+		        Sensor acelerometerSensor = listaSensores.get(0);
+		        sensorManager.registerListener(this, acelerometerSensor, SensorManager.SENSOR_DELAY_UI);
+		 }
+
+		 listaSensores = sensorManager.getSensorList(Sensor.TYPE_MAGNETIC_FIELD);
+
+		 if (!listaSensores.isEmpty()) {
+		        Sensor magneticSensor = listaSensores.get(0);
+		        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_UI);
+		 }
+
+		 listaSensores = sensorManager.getSensorList(Sensor.TYPE_TEMPERATURE);
+
+		 if (!listaSensores.isEmpty()) {
+		        Sensor temperatureSensor = listaSensores.get(0);
+		        sensorManager.registerListener(this, temperatureSensor, SensorManager.SENSOR_DELAY_UI);
+		 }
+		
+		
 	}
 
 	@Override
@@ -35,5 +72,48 @@ public class SensoresActivity extends Activity {
 	private void log (String string){
 		salida.append(string + "\n");
 	}
+		
+	/**
+	 * Implementación de los métodos 'onAccurracyChanged' y 'onSensorChanged'
+	 * de la interfaz 'SensorEventListener'
+	 */
+	 @Override 
+	 public void onAccuracyChanged(Sensor sensor, int precision) {}
+
+	  
+	 @Override
+	 public void onSensorChanged(SensorEvent evento) {
+	   //Cada sensor puede provocar que un thread principal pase por aquí
+	   //así que sincronizamos el acceso (se verá más adelante)
+	        synchronized (this) {
+	              switch(evento.sensor.getType()) {
+	              case Sensor.TYPE_ORIENTATION:
+	                     for (int i=0 ; i<3 ; i++) {
+	                            log("Orientación "+i+": "+evento.values[i]);
+	                     }
+	                     break;
+
+	              case Sensor.TYPE_ACCELEROMETER:
+	                     for (int i=0 ; i<3 ; i++) {
+	                            log("Acelerómetro "+i+": "+evento.values[i]);
+	                     }
+	                     break;
+
+	              case Sensor.TYPE_MAGNETIC_FIELD:
+	                     for (int i=0 ; i<3 ; i++) {
+	                            log("Magnetismo "+i+": "+evento.values[i]);
+	                     }
+	                     break;
+
+	              default:
+	                     for (int i=0 ; i<evento.values.length ; i++) {
+	                            log("Temperatura "+i+": "+evento.values[i]);
+	                     }
+
+	              }
+
+	        }
+
+	 }
 	
 }
